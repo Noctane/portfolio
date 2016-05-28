9dbd2bcbfee174b27af1ded433663fc77e0d5b5a
Arnaud Prat <arnaud.prat@gmail.com>
2016-05-28 10:26:46 +0200

    Jquery

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  // Nav Links
+
+  $('.menu a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('.menu li').siblings().removeClass('active');
+    $(this).parent().addClass('active');
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 200, 'swing')
+  })
+
+  // scroll system
+  $(window).scroll(function() {
+    var scrollData = $(this).scrollTop();
+    console.log(scrollData);
+
+    if (scrollData > 0) {
+      console.log('youpla');
+    }
+  });
+
+
+
+});
